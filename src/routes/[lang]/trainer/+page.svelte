452181e051384384fab44c8cbbf243a62ea42d59
Maxwell Stone <maxwell@kinetixml.com>
2023-08-05 16:50:45 -0700

    add trainer data file generator

---
+++ src/routes/[lang]/trainer/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { KMLPipeline } from 'kml-pipe-ts';
+	import { onMount } from 'svelte';
+
+	let pipe: KMLPipeline;
+	let files: FileList = [];
+	let videoSource: HTMLVideoElement;
+	let outputData: any[] = [];
+
+	onMount(async () => {
+		let { KMLPipeline } = await import('kml-pipe-ts');
+		pipe = new KMLPipeline('Outside LLMs Trainer', 1, '79705c77-f57b-449d-b856-03138e8859a7');
+		await pipe.initialize();
+	});
+	$: files.length > 0 && loadVideo(files[0]);
+	const loadVideo = async (video: File) => {
+		let url = URL.createObjectURL(video);
+		console.log(url);
+		videoSource.srcObject = null;
+		videoSource.src = url;
+		await videoSource.play();
+		processFrame();
+	};
+	const processFrame = async () => {
+		videoSource.pause();
+		let outputs = await pipe.execute([videoSource]);
+		console.log('pushed data ' + JSON.stringify(outputs[0].value));
+		outputData.push({ data: outputs[0].value, time: Date.now() });
+		outputData = outputData;
+		videoSource.requestVideoFrameCallback(processFrame);
+		videoSource.play();
+	};
+</script>
+
+<div class="h-full w-full">
+	<input type="file" bind:files />
+	<p>{outputData.length}</p>
+	<a href={`data:text/json;charset=utf-8,${JSON.stringify(outputData)}`} download="data.json"
+		>Download Data</a
+	>
+	<video id="webcam" autoplay class="rounded-lg shadow-lg" bind:this={videoSource} />
+</div>
